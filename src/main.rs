ee3775ff7431a8126640250bcdb743ec3248109a
V-Karch <luna.karch05@gmail.com>
2024-10-18 15:22:39 -0400

    tested alphabet conversion. extra spaces

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use figlet_rs::FIGfont;
 
 fn main() {
     let standard_font = FIGfont::standard().unwrap();
-    let figure = standard_font.convert("Hello Rust");
+    let figure = standard_font.convert("abcdefghijklmnopqrstuvwxyz");
     assert!(figure.is_some());
     println!("{}", figure.unwrap());
 }
