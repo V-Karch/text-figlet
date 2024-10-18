c6120ad475be9c6ecb926f316c7a1c5d5b669bea
V-Karch <luna.karch05@gmail.com>
2024-10-18 15:37:57 -0400

    Enabled accepting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use figlet_rs::FIGfont;
-
 fn main() {
-    let standard_font = FIGfont::standard().unwrap();
-    let figure = standard_font.convert("abcdefghijklmnopqrstuvwxyz");
-    assert!(figure.is_some());
-    println!("{}", figure.unwrap());
+    // Get command-line arguments and skip the first argument (the program name)
+    let args: Vec<String> = std::env::args().skip(1).collect();
+
+    // Check if exactly one argument is provided, otherwise print usage message
+    if args.len() != 1 {
+        println!("Expected 1 text argument but recieved {}", args.len());
+        println!("Usage: ./text-figlet <text-argument>");
+        return;
+    }
+
+    let input_text: &String = &args[0];
+
+    let standard_font: figlet_rs::FIGfont = figlet_rs::FIGfont::standard().unwrap();
+    let figure: figlet_rs::FIGure<'_>  = standard_font.convert(input_text).unwrap();
+
+    println!("{}", figure);
 }
